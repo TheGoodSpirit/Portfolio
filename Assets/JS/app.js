81b93dcd1bb9bc5e05a1840688873f6e90095d93
TheGoodSpirit <mannish079@gmail.com>
2023-10-06 11:22:03 +0545

    added image slider using js

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -17,20 +17,38 @@ menuToggler.addEventListener("click", () => {
   menu.classList.toggle("down");
 });
 
-
 //  Intersection observer~
 const els = document.querySelectorAll(".ani");
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    entry.target.classList.toggle("show", entry.isIntersecting);
-    // if(entry.isIntersecting) observer.unobserve(entry.target)
-  });
-}, {
-  rootMargin: "150px" ,
-  threshold : 1
-});
-
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      console.log(entry);
+      entry.target.classList.toggle("show", entry.isIntersecting);
+      // if(entry.isIntersecting) observer.unobserve(entry.target)
+    });
+  },
+  {
+    rootMargin: "150px",
+    threshold: 1,
+  }
+);
 els.forEach((el) => {
   observer.observe(el);
-})
+});
+
+// image  slider
+let slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {showSlides(slideIndex += n)}
+function currentSlide(n) {showSlides(slideIndex = n)}
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {slides[i].style.display = "none";  }
+  for (i = 0; i < dots.length; i++) {dots[i].className = dots[i].className.replace(" active", "");}
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
